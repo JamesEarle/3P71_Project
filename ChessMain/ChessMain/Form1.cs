538015c253bc592f44c8f2d1849d678d56e02dc6
James Earle <j_earle@hotmail.com>
2014-12-04 23:54:49 -0500

    Use of Byte arrays for image comparison
    Needs to be fixed, currently checking byte arrays will throw
    ArgumentNullException, despite neither bmp or A8.BackgroundImage being
    null objects.

--- ChessMain/ChessMain/Form1.cs
+++ ChessMain/ChessMain/Form1.cs
@@ -44,13 +44,69 @@ namespace ChessMain
              * swapping out the necessary images on the board.
              */ 
             //Below code successfully swaps images on A8 tile
-            if (!Boolean.Equals(A8.BackgroundImage, null))
+            /*if (!Boolean.Equals(A8.BackgroundImage, null))
             {
                 A8.BackgroundImage = null;
             }
             else
             {
                 A8.BackgroundImage = global::ChessMain.Properties.Resources.white_rook_w;
+            }*/
+
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(global::ChessMain.Properties.Resources.white_rook_w);
+
+            if (bmp == null || (Bitmap)A8.BackgroundImage == null)
+            {
+                MessageBox.Show("Why null?");
+            }
+            else
+            {
+                MessageBox.Show("not null");
+            }
+
+            byte[] a = null; // BitmapToBytes(bmp); why do these method calls have null arguments?
+            byte[] b = null; //BitmapToBytes((Bitmap)A8.BackgroundImage);
+
+            if (a != null && b != null && a.Length == b.Length)
+            {
+                int i = 0;
+                for (i=0; i < b.Length; i++)
+                {
+                    if (b[i] != a[i])
+                    {
+                        MessageBox.Show("Not eqaul");
+                    }
+                }
+                if (i == b.Length - 1)
+                {
+                    MessageBox.Show("Equal!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Not equal");
+            }
+        }
+
+        /* https://social.msdn.microsoft.com/Forums/en-US/06587f8f-c57f-492a-9616-4b1b8e8b7d35/check-the-image-used-in-a-picturebox?forum=Vsexpressvcs */
+        public static byte[] BitmapToBytes(Bitmap Bitmap)
+        {
+            System.IO.MemoryStream ms = null;
+            try
+            {
+                ms = new System.IO.MemoryStream();
+                Bitmap.Save(ms, Bitmap.RawFormat);
+                byte[] byteImage = new Byte[ms.Length];
+                byteImage = ms.ToArray();
+                return byteImage;
+            }
+            catch(System.ArgumentNullException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                ms.Close();
             }
         }
 
